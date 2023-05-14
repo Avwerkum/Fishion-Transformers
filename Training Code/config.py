805080b704eb5f7d4ecf79b990aa2bda324f77f8
Avwerkum <117994052+Avwerkum@users.noreply.github.com>
2023-05-14 18:47:34 +0200

    Update config.py

--- Training Code/config.py
+++ Training Code/config.py
@@ -1,7 +1,14 @@
-# Config
+# This file is used to configure the training scripts.
+# Adjust parameters and settings before running each script.
 
+#Import two dependencies
 import timm
 import torch
+
+# Models
+ViTB16 =  timm.create_model('vit_base_patch16_224', pretrained=True)
+efficientnet_b2 = timm.create_model('efficientnet_b2', pretrained=True)
+deit_base_patch16_224 = torch.hub.load('facebookresearch/deit:main', 'deit_base_patch16_224', pretrained=True)
 
 #Set data locations
 TRAIN_DIR = 
@@ -11,7 +18,7 @@ VAL_DIR =
 #Set save location
 SAVE_DIR =
 
-
+# Adjust 
 BATCH_SIZE = 32
 NUM_WORKERS = 0
 
@@ -20,7 +27,6 @@ NUM_CLASSES = 30
 #Hyperparameters
 LEARNING_RATE = 1e-4
 WEIGHT_DECAY = 0.03
-
 NUM_EPOCHS = 100
 
 #CosineAnnealing
@@ -30,8 +36,3 @@ ETA_MIN = 1e-6
 #Early Stopping Criteria
 PATIENCE = 10
 DELTA = 0.001
-
-# Models
-ViTB16 =  timm.create_model('vit_base_patch16_224', pretrained=True)
-efficientnet_b2 = timm.create_model('efficientnet_b2', pretrained=True)
-deit_base_patch16_224 = torch.hub.load('facebookresearch/deit:main', 'deit_base_patch16_224', pretrained=True)
